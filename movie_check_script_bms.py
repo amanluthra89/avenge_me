c4a226073edcc6cb786911d872230aa3df38b87c
Aman Luthra <aman.luthra@ril.com>
2019-04-20 21:08:54 +0530

    Added check for IMAX and 3D

--- movie_check_script_bms.py
+++ movie_check_script_bms.py
@@ -7,7 +7,7 @@ import random
 city_arr = ["mumbai", "bengaluru", "kolhapur"]
 site = "https://in.bookmyshow.com/mumbai/movies/avengers-endgame/ET00090482"
 
-delay = 900
+delay = 600
 SAD_DIALOGUES = ["I don't feel so good Mr Stark.", "I am Groot", "You are not the only one cursed with knowledge.", 
 "I hope they remember you", "All words are made up.", "When Im done, half of humanity will still exist.", 
 "I assure you, brother, the sun will shine on us again.", "The hardest choices require the strongest of wills.", 
@@ -15,7 +15,7 @@ SAD_DIALOGUES = ["I don't feel so good Mr Stark.", "I am Groot", "You are not th
 "I wouldnt say no to a Tuna Melt.", "Broke up? Like a band? Like the Beatles?", "Never tell me the odds - whoops, wrong movie",
 "Why is Gamora?", "The rabbit is correct, and clearly the smartest one.", "Ive mastered the ability of standing so incredibly still that Ive become invisible to the eye.",
 "Theres an Ant-Man and a Spider-Man?", "Oh. We are using our made-up names.", "Kick names, take ass.", "Exactly like Footloose. Is it still the greatest movie in history?",
-"Fourteen million six hundred and five."]
+"Fourteen million six hundred and five.", "Hey guys, you ever see that really old movie, Empire Strikes Back? When they're on the snow planet? With the walking thingies?"]
 
 def telegram_bot_sendtext(msg):
     bot_token = '654480132:AAElQU1AhEz269a-dADYsBDi7fw04YnMiPg'
@@ -31,10 +31,20 @@ while True:
     page = urllib.urlopen(req)
     soup = BeautifulSoup(page)
     soup2 = soup.find_all('div', {'action-book'})
+    imax_soup = soup.find_all('div' ,{'format-dimensions'})
     message = city + " - " + random.choice(SAD_DIALOGUES) + " --- No tickets yet."
     for div in soup2:
       if(div.find('a') != None):
         if 'Book' in div.find('a').contents[0]:
           message = city + " -- Tickets available! --- We're in the Endgame now!! Go Go Go!!!"
-    telegram_bot_sendtext(message)
+    for div in imax_soup:
+      for i in div.find_all('a'):
+        if 'IMAX' in i.contents[0]:
+          message = message + ' --- IMAX!!!!'
+          break
+        if '3D' in div.find('a').contents[0]:
+          message = message + ' --- 3D'
+          break
+    # telegram_bot_sendtext(message)
+    print(message)
   time.sleep(delay)
