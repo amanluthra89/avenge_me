9207eaef9316a3e4f2742ec07aae0f231d9c6730
Aman Luthra <aman.luthra@ril.com>
2019-04-21 02:45:15 +0530

    Just PVR checks

--- movie_check_script_bms.py
+++ movie_check_script_bms.py
@@ -7,7 +7,7 @@ import random
 city_arr = ["mumbai"]
 site = "https://in.bookmyshow.com/mumbai/movies/avengers-endgame/ET00090482"
 
-delay = 30
+delay = 60
 SAD_DIALOGUES = ["I don't feel so good Mr Stark.", "I am Groot", "You are not the only one cursed with knowledge.", 
 "I hope they remember you", "All words are made up.", "When Im done, half of humanity will still exist.", 
 "I assure you, brother, the sun will shine on us again.", "The hardest choices require the strongest of wills.", 
@@ -25,35 +25,47 @@ def telegram_bot_sendtext(msg):
     return response.json()
 
 while True:
-  for city in city_arr:
-    site = "https://in.bookmyshow.com/"+city+"/movies/avengers-endgame/ET00090482"
-    req = urllib.Request(site, headers={'User-Agent': 'Mozilla/5.0'})
-    page = urllib.urlopen(req)
-    soup = BeautifulSoup(page)
-    soup2 = soup.find_all('div', {'action-book'})
-    imax_soup = soup.find_all('div' ,{'format-dimensions'})
-    message = city + " - " + random.choice(SAD_DIALOGUES) + " --- No tickets yet."
-    for div in soup2:
-      if(div.find('a') != None):
-        if 'Book' in div.find('a').contents[0]:
-          message = city + " -- Tickets available! --- We're in the Endgame now!! Go Go Go!!!"
-    for div in imax_soup:
-      for i in div.find_all('a'):
-        if 'IMAX' in i.contents[0]:
-          message = message + ' --- IMAX!!!!'
-          telegram_bot_sendtext(message)
-          break
-        if '3D' in div.find('a').contents[0]:
-          message = message + ' --- 3D'
-          break
-    print(message)
-    site = "https://in.bookmyshow.com/buytickets/avengers-endgame-bengaluru/movie-bang-ET00100668-MT/20190426"
-    req = urllib.Request(site, headers={'User-Agent': 'Mozilla/5.0'})
-    page = urllib.urlopen(req)
-    soup = BeautifulSoup(page)
-    s = soup.find_all('div', {'__name'})
-    for venue in s:
-        string_venue = str(venue.find('a').contents[1])
-        if 'PVR' in string_venue:
-            telegram_bot_sendtext("PVR BLR IMAX DEFCON RED")
+  site = "https://in.bookmyshow.com/buytickets/avengers-endgame-bengaluru/movie-bang-ET00100668-MT/20190426"
+  req = urllib.Request(site, headers={'User-Agent': 'Mozilla/5.0'})
+  page = urllib.urlopen(req)
+  soup = BeautifulSoup(page)
+  s = soup.find_all('div', {'__name'})
+  for venue in s:
+      string_venue = str(venue.find('a').contents[1])
+      print(string_venue)
+      if 'PVR' in string_venue:
+          telegram_bot_sendtext("PVR BLR IMAX DEFCON RED")
+  site = "https://in.bookmyshow.com/buytickets/avengers-endgame-kolhapur/movie-kolh-ET00100559-MT/20190426"
+  req = urllib.Request(site, headers={'User-Agent': 'Mozilla/5.0'})
+  page = urllib.urlopen(req)
+  soup = BeautifulSoup(page)
+  s = soup.find_all('div', {'__name'})
+  for venue in s:
+      string_venue = str(venue.find('a').contents[1])
+      print(string_venue)
+      if 'PVR' in string_venue:
+          telegram_bot_sendtext("PVR KOLHAPUR AVAILABLE - GO GO GO")    
   time.sleep(delay)
+  # for city in city_arr:
+  #   site = "https://in.bookmyshow.com/"+city+"/movies/avengers-endgame/ET00090482"
+  #   req = urllib.Request(site, headers={'User-Agent': 'Mozilla/5.0'})
+  #   page = urllib.urlopen(req)
+  #   soup = BeautifulSoup(page)
+  #   soup2 = soup.find_all('div', {'action-book'})
+  #   imax_soup = soup.find_all('div' ,{'format-dimensions'})
+  #   message = city + " - " + random.choice(SAD_DIALOGUES) + " --- No tickets yet."
+    # for div in soup2:
+    #   if(div.find('a') != None):
+    #     if 'Book' in div.find('a').contents[0]:
+    #       message = city + " -- Tickets available! --- We're in the Endgame now!! Go Go Go!!!"
+    # for div in imax_soup:
+    #   for i in div.find_all('a'):
+    #     if 'IMAX' in i.contents[0]:
+    #       message = message + ' --- IMAX!!!!'
+    #       telegram_bot_sendtext(message)
+    #       break
+    #     if '3D' in div.find('a').contents[0]:
+    #       message = message + ' --- 3D'
+    #       break
+    # print(message)
+  
